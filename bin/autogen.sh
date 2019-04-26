9cbc9d160cd3b332f454f4af0337d048ca03f8f3
Craig Quiter <cquiter@gmail.com>
2019-04-25 21:12:37 -0700

    Generate leaderboard on start autogen watcher

--- bin/autogen.sh
+++ bin/autogen.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+gen_cmd="python leaderboard_generator/gen.py"
+
+# Generate on start
+eval "${gen_cmd}"
+
+watch_cmd="echo file changed \${watch_src_path}; ${gen_cmd}"
+
+echo watch_cmd "${watch_cmd}"
+
+# Generate on file change
 watchmedo shell-command \
-  --ignore-pattern="*/generated*;*/.idea*;*___*;" \
+  --ignore-pattern="*/generated*;*/.idea*;*___*;./.git*" \
   --recursive \
-  --command='echo file changed "${watch_src_path}"; python leaderboard_generator/gen.py' \
+  --command="${watch_cmd}"
   --wait \
   --drop \
   --interval 5.0 \
