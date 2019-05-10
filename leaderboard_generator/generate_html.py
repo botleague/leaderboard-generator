89ff058bb6e8ecfec5fe636a69eb9f2300dd825d
Craig Quiter <cquiter@gmail.com>
2019-05-09 20:07:36 -0700

    Code complete pull problem from botleague

--- leaderboard_generator/generate_html.py
+++ leaderboard_generator/generate_html.py
@@ -7,18 +7,21 @@ import json
 import time
 from urllib.parse import urlparse
 
+import github
 import requests
 from future.builtins import (dict)
 
-import os
 import os.path as p
 import shutil
 from distutils.dir_util import copy_tree
+
+from github import Github
 from jinja2 import Environment, PackageLoader, select_autoescape
 from leaderboard_generator import logs
 
 import leaderboard_generator.constants as c
-from leaderboard_generator.util import load_json
+from leaderboard_generator.botleague_gcp.constants import GITHUB_TOKEN
+from leaderboard_generator.util import load_json, write_file, read_file
 
 log = logs.get_log(__name__)
 
@@ -28,35 +31,15 @@ APP = 'leaderboard'
 APP_DIR = p.join(DIR, APP)
 
 
-def add_youtube_embed(submissions):
-    for submission in submissions:
-        url = submission['youtube']
-        submission['youtube_embed'] = get_youtube_embed_url(url)
-
-
-def get_youtube_embed_url(url):
-    parsed = urlparse(url)
-    query_dict = urllib.parse.parse_qs(parsed.query)
-    if 'v' in query_dict:
-        embed_url = '%s://%s/embed/%s' % (parsed.scheme, parsed.netloc,
-                                          query_dict['v'][0])
-    else:
-        embed_url = url
-    return embed_url
-
-
-def get_problem_def(problem_id):
-    # Get latest problem definition json from GitHub
-
-    pass
-
-
 class HtmlGenerator:
     def __init__(self):
         self.env = Environment(
             loader=PackageLoader(APP, 'templates'),
             autoescape=select_autoescape(['html', 'xml']))
         self.last_run = -1
+        github.enable_console_debug_logging()
+        self.github = Github(GITHUB_TOKEN)
+        self.botleague_repo = self.github.get_repo('deepdrive/botleague')
 
     def regenerate_html(self):
         if self.last_run != -1 and time.time() - self.last_run < 1:
@@ -88,21 +71,52 @@ class HtmlGenerator:
 
         # Get our Jinja template
         template = self.env.get_template('problem_leaderboard.html')
+
+        # Generate leaderboard for each problem
         problem_files = glob(c.LEADERBOARD_DIR + '/data/problems/*.json')
         for filename in problem_files:
-            # TODO: Get problem definition from botleague
             results = load_json(filename)
             if results:
                 problem_id = results['bots'][0]['problem']
-                problem_def = get_problem_def(problem_id)
+                problem_def, readme = self.get_problem_def(problem_id)
                 out_filename = p.join(c.GEN_DIR, 'problems',
                                       problem_id + '.html')
                 submissions = results['bots']
                 add_youtube_embed(submissions)
                 write_template(out_filename, template, data=dict(
-                    problem_name=problem_id,
+                    problem_name=problem_def['display_name'],
+                    problem_readme=readme,
                     problem_video='https://www.youtube.com/embed/ALdsqfrLieg',
                     submissions=submissions))
+
+    def get_problem_def(self, problem_id):
+        path = p.join(c.PROBLEM_DEFINITIONS_DIR, problem_id)
+        problem_filename = 'problem.json'
+        readme_filename = 'README.md'
+        problem_path = path + '/' + problem_filename
+        readme_path = path + '/' + readme_filename
+        if p.exists(path):
+            definition = load_json(problem_path)
+            readme = read_file(readme_path)
+        else:
+            # New problem, get definition json from botleague
+            # TODO: Don't allow this if submissions have been made by
+            #  authors other than creator
+            problem_dir = 'problems/%s' % problem_id
+            definition_str = self.get_file_from_github(
+                problem_dir + '/' + problem_filename)
+            definition = json.loads(definition_str)
+            readme = self.get_file_from_github(
+                problem_dir + '/' + readme_filename)
+            write_file(definition_str, problem_path)
+            write_file(readme, readme_path)
+
+        return definition, readme
+
+    def get_file_from_github(self, path):
+        contents = self.botleague_repo.get_contents(path)
+        content_str = contents.decoded_content
+        return content_str
 
     @staticmethod
     def create_clean_gen_dir():
@@ -114,6 +128,24 @@ class HtmlGenerator:
 def write_template(out_html_filename, template, data):
     with open(out_html_filename, 'w') as outfile:
         outfile.write(template.render(data))
+
+
+def add_youtube_embed(submissions):
+    for submission in submissions:
+        url = submission['youtube']
+        submission['youtube_embed'] = get_youtube_embed_url(url)
+
+
+def get_youtube_embed_url(url):
+    parsed = urlparse(url)
+    query_dict = urllib.parse.parse_qs(parsed.query)
+    if 'v' in query_dict:
+        embed_url = '%s://%s/embed/%s' % (parsed.scheme, parsed.netloc,
+                                          query_dict['v'][0])
+    else:
+        embed_url = url
+    return embed_url
+
 
 
 if __name__ == '__main__':
--- leaderboard_generator/util.py
+++ leaderboard_generator/util.py
@@ -5,3 +5,14 @@ def load_json(filename):
     with open(filename) as file:
         results = json.load(file)
     return results
+
+
+def write_file(content, path):
+    with open(path, 'w') as f:
+        f.write(content)
+
+
+def read_file(path):
+    with open(path) as f:
+        ret = f.read()
+    return ret
