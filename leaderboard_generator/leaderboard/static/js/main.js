49ffb9d4bb3c6e0edc8c260039f6e4fb177ccb17
Craig Quiter <cquiter@gmail.com>
2019-07-25 17:58:38 -0700

    Format timeSince

--- leaderboard_generator/leaderboard/static/js/main.js
+++ leaderboard_generator/leaderboard/static/js/main.js
@@ -1,33 +1,33 @@
 'use strict';
 
 (function () {
-  function timeSince(date) {
+    function timeSince(date) {
 
-    const seconds = Math.floor((new Date() / 1000 - date));
+        const seconds = Math.floor((new Date() / 1000 - date));
 
-    let interval = Math.floor(seconds / 31536000);
+        let interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) {
-      return interval + " years";
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
     }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-      return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-      return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-      return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-      return interval + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
-  }
 
     const abbreviate_number = function (num, fixed) {
         if (num === null) {
