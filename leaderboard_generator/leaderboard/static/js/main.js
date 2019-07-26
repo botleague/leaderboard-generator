3a89102eaab785253e3906bef08f9e47001c337b
Craig Quiter <cquiter@gmail.com>
2019-07-25 17:58:17 -0700

    Abbreviate large stats

--- leaderboard_generator/leaderboard/static/js/main.js
+++ leaderboard_generator/leaderboard/static/js/main.js
@@ -29,17 +29,41 @@
     return Math.floor(seconds) + " seconds";
   }
 
-  jQuery(function ($) {
-    console.log('jquery loaded');
-    $('.time').each(function (_) {
-      let time = $(this).text();
-      $(this).text(timeSince(time) + ' ago');
+    const abbreviate_number = function (num, fixed) {
+        if (num === null) {
+            return null;
+        } // terminate early
+        if (num === 0) {
+            return '0';
+        } // terminate early
+        fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+        const b = (num).toPrecision(2).split("e"), // get power
+            k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+            c = k < 1 ? num.toFixed(fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+            d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+            e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+        return e;
+    };
+
+    jQuery(function ($) {
+        console.log('jquery loaded');
+        $('.time').each(function (_) {
+            let time = $(this).text();
+            console.log(time);
+
+            $(this).text(timeSince(time) + ' ago');
+        });
+
+        $('.score-value, .trip-speed-value').each(function (_) {
+            let number = parseFloat($(this).text());
+            $(this).text(abbreviate_number(number));
+        });
+
+        $('.stats').css({visibility: "visible"});
     });
-});
 
 
 })();
-
 
 
 // var aDay = 24 * 60 * 60 * 1000;
