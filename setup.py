290339e90d08bc29adb6993ceb12d644ae055e4b
Craig Quiter <cquiter@gmail.com>
2019-08-21 13:19:58 -0700

    Handle git requires in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
 import os
 from os.path import abspath, dirname, join
-from typing import List
+from typing import List, Tuple
 
 from setuptools import setup
 
 
-def get_requires() -> List[str]:
+def get_requires() -> Tuple[List[str], List[str]]:
     with open('requirements.txt') as reqs_file:
-        reqs = reqs_file.read().split(os.sep)
-    return reqs
+        reqs = reqs_file.read().split(os.linesep)
+    dep_links = []
+    final_reqs = []
+    for req in reqs:
+        if not req.startswith('git+git://github.com/'):
+            final_reqs.append(req)
+        else:
+            req = req.replace('git+git', 'https')
+            url, egg = req.split('#egg=')
+            dep_link = url + '/tarball/master'
+            if egg:
+                dep_link += '#egg=' + egg
+            dep_links.append(dep_link)
+    return final_reqs, dep_links
 
 
 # Read the README markdown data from README.md
 with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readme_file:
     __readme__ = readme_file.read().decode('utf-8')
+
+requires, dependency_links = get_requires()
 
 setup(
     name='leaderboard-generator',
@@ -34,5 +48,6 @@ setup(
     packages=['leaderboard_generator'],
     zip_safe=True,
     python_requires='>=3.7',
-    install_requires=get_requires()
+    install_requires=requires,
+    dependency_links=dependency_links,
 )
